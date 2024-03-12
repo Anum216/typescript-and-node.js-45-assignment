d2a97d03bffa659155db89eb69059d00900428df
Anum216 <163039263+Anum216@users.noreply.github.com>
2024-03-12 22:25:04 +0500

    Add files via upload

---
+++ 25_alien_color_1.ts
@@ -0,0 +1,11 @@
+let alien_color:string ='green';
+
+if (alien_color === 'green'){
+    console.log("player just earned 5 point");
+}
+
+alien_color ='yellow';
+
+if (alien_color === 'green'){
+    console.log("player just earned 5 point");
+}
