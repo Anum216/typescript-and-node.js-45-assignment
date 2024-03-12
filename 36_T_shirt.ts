1ea2aa09f60c75cb436680ba24a93d7da74b193c
Anum216 <163039263+Anum216@users.noreply.github.com>
2024-03-12 22:47:48 +0500

    Add files via upload

---
+++ 36_T_shirt.ts
@@ -0,0 +1,8 @@
+function make_shirt(size:string,text:string){
+    console.log(`Creating a ${size} shirt with the message: ${text}`);
+}
+make_shirt('Large', 'Hello world')
+make_shirt('medium', 'Hello world')
+make_shirt('small', 'Hello world')
+
+
