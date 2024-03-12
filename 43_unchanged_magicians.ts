2e94c7a2d02946dda4a0e6e852577a2a65ed4d0b
Anum216 <163039263+Anum216@users.noreply.github.com>
2024-03-12 23:02:19 +0500

    Add files via upload

---
+++ 43_unchanged_magicians.ts
@@ -0,0 +1,27 @@
+function show_magicians(magicians: string[]): void{
+    for (const magician of magicians) {
+       console.log(magician.charAt(0).toUpperCase() + magician.slice(1));
+    }
+}
+    function make_great(magicians:string[]): void {
+   for (let i = 0; i < magicians.length; i++){
+       magicians[i] = magicians[i] + ' the great'
+   }
+}
+const magicians2: string[] = ["usman","haseeb","wajahat"];
+make_great(magicians2)
+show_magicians(magicians2)
+
+
+
+function make_great2(magicians: string[]): string[] {
+    const greatmagicians: string[] = [];
+    for(let i=0; i < magicians.length; i++){
+        greatmagicians.push(magicians[i] + 'the great');
+    }
+    return greatmagicians;
+}
+const magicians3:string[] = ["usman","haseeb","wajahat"];
+const greatmagicians2: string[] = make_great2(magicians3);
+show_magicians(magicians3);
+show_magicians(greatmagicians2);
