72b7fb8c32c423ba1122274d372dafb8f528b7f0
Anum216 <163039263+Anum216@users.noreply.github.com>
2024-03-12 22:16:09 +0500

    Add files via upload

---
+++ 24_m_condition.ts
@@ -0,0 +1,32 @@
+
+console.log("inequality test with strings:",("apple"  as string) !="orange");
+console.log("lower.case function test:", "Hello".toLowerCase() === "hello");
+
+console.log("equality test with number",26===26);
+
+console.log("greater than test", 10>5);
+
+console.log("greater than  and equal to", 10>=10);
+
+console.log("greater than  and equal to", 5<=10);
+
+console.log("operator test.", 5===5 || false);
+
+console.log("operator test.", 5===5 && 10>5);
+
+const fruits:string[] = ['nashpati', 'banana','amround'];
+console.log('test "nashpati" in the array' , fruits.includes("nashpati"));
+
+console.log('test "apple" is not the array' , !fruits.includes("apple"));
+
+
+
+
+
+
+
+
+
+
+
+
