240ac0a33d628feb6b8b468a7f04231b256232dd
Anum216 <163039263+Anum216@users.noreply.github.com>
2024-03-12 23:04:25 +0500

    Add files via upload

---
+++ 44_sandwiches.ts
@@ -0,0 +1,12 @@
+function sandwich(...items: string[]): void {
+    console.log("sandwich order:")
+
+    for(let i = 0; i < items.length; i++){
+        console.log(` - ${items[i]}`)
+    }
+}
+console.log("enjoy your sandwich anum awan")
+
+sandwich('capsicum' , 'tomato' , 'chicken')
+sandwich('beef' , 'cheese')
+sandwich('garlic chicken' , 'mayo sauce')
