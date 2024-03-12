c948560f9648ad5d7a19c578b3835acc16af6640
Anum216 <163039263+Anum216@users.noreply.github.com>
2024-03-12 22:27:02 +0500

    Add files via upload

---
+++ 26_alien_color2.ts
@@ -0,0 +1,15 @@
+let aliencolors:string = "green";
+if(aliencolors === "green"){
+    console.log("player just earned 5 points for shooting the alien.");
+}else{
+    console.log("player just earned 10 point.")
+}
+
+ aliencolors ="yellow";
+if(aliencolors === "green"){
+    console.log("player just earned 5 points fro shooting the alien.");
+}else{
+    console.log("player just earned 10 point.")
+}
+
+
