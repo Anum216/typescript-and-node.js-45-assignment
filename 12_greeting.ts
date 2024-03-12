7c91481cac4b1384d07168d2a778b853eca9fd6a
Anum216 <163039263+Anum216@users.noreply.github.com>
2024-03-12 21:41:35 +0500

    Add files via upload

---
+++ 12_greeting.ts
@@ -0,0 +1,5 @@
+let names : string[] = ['talha', 'Asad', 'mateen', 'ejaz',];
+let message : string = 'welcome to our class javascript: '
+for(let i=0; i<names.length; i++){
+    console.log( message + names[i]);
+}
