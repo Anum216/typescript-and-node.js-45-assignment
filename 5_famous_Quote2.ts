dedc4347e81cb91cf4e95f17ad61eb6ce830b356
Anum216 <163039263+Anum216@users.noreply.github.com>
2024-03-12 21:08:55 +0500

    Add files via upload

---
+++ 5_famous_Quote2.ts
@@ -0,0 +1,3 @@
+let famous_Quote : string=' Albert Eintain';
+let message :string='"A person who never made a mistake never tried anything new."';
+console.log(`${famous_Quote} once said,${message}`)
