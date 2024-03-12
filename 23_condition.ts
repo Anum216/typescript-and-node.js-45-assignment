c9c7f5c1f527dbd6e9be8511ab9148c534e1f03b
Anum216 <163039263+Anum216@users.noreply.github.com>
2024-03-12 22:13:38 +0500

    Add files via upload

---
+++ 23_condition.ts
@@ -0,0 +1,35 @@
+let car : string = 'subaru';
+console.log("is car === 'subaru' I predict true.");
+console.log(car ==='subaru');
+
+console.log("is car == 'subaru' I predict true.");
+console.log(car =='subaru');
+
+console.log("is car != 'honda' I predict true.");
+console.log(car != 'honda');
+
+console.log("is car !== 'ford' I predict true.");
+console.log(car !=='ford');
+
+console.log("is car.uppercase == 'SUBARU' I predict true.");
+console.log(car.toUpperCase() =='SUBARU');
+
+console.log("is car == 'SUBARU'? I predict false.");
+console.log(car =='SUBARU');
+
+console.log("is car === 'SUBARU'? I predict false.");
+console.log(car ==='SUBARU');
+
+console.log("is car == 'Train'? I predict false.");
+console.log(car ==='Train');
+
+console.log("is car == 'Bus'? I predict false.");
+console.log(car ==='Bus');
+
+console.log("is car == 'Cycle'? I predict false.");
+console.log(car ==='Cycle');
+
+
+
+
+
