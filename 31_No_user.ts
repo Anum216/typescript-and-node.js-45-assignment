aa46b55a3f6610dd66cf6beeefdcffa59559f27c
Anum216 <163039263+Anum216@users.noreply.github.com>
2024-03-12 22:38:12 +0500

    Add files via upload

---
+++ 31_No_user.ts
@@ -0,0 +1,7 @@
+let userNames: string[] = [ 'Admin','junaid','Zeehan','Kamran','Daniyal']
+if(userNames.length===0){
+    console.log('we need to find some user')
+}else{
+    userNames =[];
+    console.log('All user name has been removed.'+ userNames.length)
+}
