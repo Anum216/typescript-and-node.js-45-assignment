d6519e2cf38647224db57ab87f7b37d3d2116e29
Anum216 <163039263+Anum216@users.noreply.github.com>
2024-03-12 22:46:01 +0500

    Add files via upload

---
+++ 35_Animals.ts
@@ -0,0 +1,5 @@
+let Animals : string[] = ['dog','cat','rabbit'];
+for(let animal of Animals){
+    console.log(`A ${animal} would make a great pet.`);
+}
+console.log(`any of these animals would make a great pet!`);
