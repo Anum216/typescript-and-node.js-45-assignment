e909e5894d623615f6fa217b56f75818f64ebdbb
Anum216 <163039263+Anum216@users.noreply.github.com>
2024-03-12 22:09:08 +0500

    Add files via upload

---
+++ 21_object.ts
@@ -0,0 +1,16 @@
+interface person{
+    age : number ,
+    name  : string  ,
+    nationality : string ,
+    students : boolean 
+}
+
+
+
+let person: person ={
+    age :26,
+    name :'anum' ,
+    nationality : 'pakistani' ,
+    students : true ,
+}
+console.log(person);
