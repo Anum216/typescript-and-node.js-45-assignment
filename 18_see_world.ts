fd22c9a660eac58997518978a7b48c7487f033b0
Anum216 <163039263+Anum216@users.noreply.github.com>
2024-03-12 22:00:24 +0500

    Add files via upload

---
+++ 18_see_world.ts
@@ -0,0 +1,17 @@
+let places :string[] = ['italy', 'germany', 'agra','eifel tower', 'times square'];
+console.log('original'+places)
+console.log('copy' + [...places].sort());
+console.log('original' + places);
+console.log('copy' + [...places].sort().reverse());
+console.log('original' + places);
+console.log('origin' + places.reverse());
+console.log('origin' + places.sort());
+console.log('origin' + places.sort ().reverse());
+
+
+
+
+
+
+
+
