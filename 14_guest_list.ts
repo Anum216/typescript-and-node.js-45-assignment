e765f91920dd4b4c7aff443c33e6f9dc2e3d4df5
Anum216 <163039263+Anum216@users.noreply.github.com>
2024-03-12 21:46:01 +0500

    Add files via upload

---
+++ 14_guest_list.ts
@@ -0,0 +1,6 @@
+let guest_list:string[] = ['ali','elict','huzaifa']
+for(let i=0;i<guest_list.length; i++){
+
+
+console.log ('Dear Mr. '+ guest_list[i] +',\n\nit is our pleasure to invite you  in our party.\n\nthank you!')
+}
